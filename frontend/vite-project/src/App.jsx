52ff3a8d28a69bf801bd932ebf5ef44b363cb9b7
ram <rramkumar1306@gmail.com>
2024-04-11 23:43:54 +0530

    Introduced navBar.

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NavBar from './components/NavBar.jsx'
 
 function App() {
 
   return (
-    <>
-      <div className="text-3xl font-bold underline">
-        HELLO EVERYONE
-      </div>
+    <div className='flex flex-col min-h-screen bg-gray100 font-sans antialiased'>
+      <NavBar/>
   
-    </>
+    </div>
   )
 }
 
---
+++ frontend/vite-project/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+
+const NavBar = ()=>{
+
+    return (
+    <nav className="flex justify-center bg-green-300 text-white">
+        <div>
+            <h1 className="text-3xl">RAIK Blog Generation</h1>
+        </div>
+        <div>
+            <a className="text-white">Logout</a>
+        </div>
+
+
+    </nav>)
+}
+
+export default NavBar
