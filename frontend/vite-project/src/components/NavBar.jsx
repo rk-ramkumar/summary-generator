8f7684ada122381517911795bbdb674197ab9b2e
ram <rramkumar1306@gmail.com>
2024-04-11 23:52:59 +0530

    Changed alignment.

--- frontend/vite-project/src/components/NavBar.jsx
+++ frontend/vite-project/src/components/NavBar.jsx
@@ -2,15 +2,13 @@
 const NavBar = ()=>{
 
     return (
-    <nav className="flex justify-between items-center bg-green-300 text-white">
+    <nav className="flex justify-around items-center bg-green-600 text-white h-24">
         <div>
             <h1 className="text-3xl font-bold">RAIK Blog Generation</h1>
         </div>
         <div>
             <a href ="#" className="text-white">Logout</a>
         </div>
-
-
     </nav>)
 }
 
