095b7618607818c0898e412a63f3ee65e05c76ce
ram <rramkumar1306@gmail.com>
2024-04-11 23:47:54 +0530

    Refactored.

--- frontend/vite-project/src/components/NavBar.jsx
+++ frontend/vite-project/src/components/NavBar.jsx
@@ -2,12 +2,12 @@
 const NavBar = ()=>{
 
     return (
-    <nav className="flex justify-center bg-green-300 text-white">
+    <nav className="flex justify-between items-center bg-green-300 text-white">
         <div>
-            <h1 className="text-3xl">RAIK Blog Generation</h1>
+            <h1 className="text-3xl font-bold">RAIK Blog Generation</h1>
         </div>
         <div>
-            <a className="text-white">Logout</a>
+            <a href ="#" className="text-white">Logout</a>
         </div>
 
 
