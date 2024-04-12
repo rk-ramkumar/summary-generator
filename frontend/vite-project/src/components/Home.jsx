48fcffad75dc7b961c02079e5b8b7ca22df688e5
ram <rramkumar1306@gmail.com>
2024-04-13 02:55:32 +0530

    Added loading to home.

--- frontend/vite-project/src/components/Home.jsx
+++ frontend/vite-project/src/components/Home.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import homePage from "../contents/homePage.json";
 import Summary from "./Summary";
+import Loading from "../components/Loading";
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div className="flex-grow container mx-auto mt-10 px-4 sm:px-0">
       <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105 flex flex-col">
@@ -10,16 +12,26 @@ const Home = () => {
           <h2 className="text-2xl font-semibold mb-4">{homePage.title}</h2>
           <p className="text-gray-800">{homePage.description}</p>
         </div>
-        
+
         <div className="mt-6">
-          <h2 className="text-xl font-semibold mb-4">Enter Youtube Video Link </h2>
+          <h2 className="text-xl font-semibold mb-4">
+            Enter Youtube Video Link{" "}
+          </h2>
           <div className="flex space-x-4">
-            <input id="youtubeLink" type="url" placeholder="Paste Youtube Link" className="flex-grow p-4 border border-blue-900 rounded-lg"/>
-            <button id="generateBtn" className="hover:bg-[#3A3A3A]">Generate</button>
+            <input
+              id="youtubeLink"
+              type="url"
+              placeholder="Paste Youtube Link"
+              className="flex-grow p-4 border border-blue-900 rounded-lg"
+            />
+            <button id="generateBtn" className="hover:bg-[#3A3A3A]">
+              Generate
+            </button>
           </div>
         </div>
 
-        <Summary/>
+        <Loading isLoading={isLoading}/>
+        <Summary />
       </div>
     </div>
   );
