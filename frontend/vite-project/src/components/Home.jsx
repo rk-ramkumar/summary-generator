754fe859792994d6f5384c9c6430f33cac242a92
ram <rramkumar1306@gmail.com>
2024-04-13 01:42:17 +0530

    Refactored.

--- frontend/vite-project/src/components/Home.jsx
+++ frontend/vite-project/src/components/Home.jsx
@@ -4,7 +4,7 @@ import homePage from "../contents/homePage.json";
 const Home = () => {
   return (
     <div className="flex-grow container mx-auto mt-10 px-4 sm:px-0">
-      <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow">
+      <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105 flex flex-col">
         <div className="text-center">
           <h2 className="text-2xl font-semibold mb-4">{homePage.title}</h2>
           <p className="text-gray-800">{homePage.description}</p>
--- frontend/vite-project/src/components/NavBar.jsx
+++ frontend/vite-project/src/components/NavBar.jsx
@@ -2,9 +2,9 @@
 const NavBar = ()=>{
 
     return (
-    <nav className="flex justify-around items-center  text-white h-24 sticky top-0 backdrop-blur-sm bg-black/30">
+    <nav className="flex justify-around items-center  text-white h-24 sticky top-0 backdrop-blur-sm bg-[#1C1678]/70">
         <div>
-            <h1 className="text-3xl font-bold cursor-pointer hover:text-[#42b883]">RAIK Blog Generator</h1>
+            <h1 className="text-3xl font-bold cursor-pointer hover:text-[#42b883]">RKAI Blog Generator</h1>
         </div>
         <div>
             <a href ="#" className="text-white">Logout</a>
