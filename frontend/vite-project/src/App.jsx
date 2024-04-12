24626b8595eb766e8e31d5b8928499e4ccb0f443
ram <rramkumar1306@gmail.com>
2024-04-13 00:48:28 +0530

    Added Home to app main.

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,3 +1,4 @@
+import Home from './components/Home.jsx'
 import NavBar from './components/NavBar.jsx'
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
   return (
     <div className='flex flex-col min-h-screen bg-gray100 font-sans antialiased'>
       <NavBar/>
-  
+      <Home/>
     </div>
   )
 }
