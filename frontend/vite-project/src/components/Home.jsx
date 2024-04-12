8f4410084f500bce53b7b5d33f7729bcbc4605fb
ram <rramkumar1306@gmail.com>
2024-04-13 00:48:03 +0530

    Introduced Home component.

---
+++ frontend/vite-project/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import homePage from "../contents/homePage.json";
+
+const Home = () => {
+  return (
+    <div className="flex-grow container mx-auto mt-10 px-4 sm:px-0">
+      <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow">
+        <div className="text-center">
+          <h2 className="text-2xl font-semibold mb-4">{homePage.title}</h2>
+          <p className="text-gray-300">{homePage.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
