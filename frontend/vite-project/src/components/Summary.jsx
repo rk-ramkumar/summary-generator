fcbf94aecd880b3ed98e5408ebad683db3d3454e
ram <rramkumar1306@gmail.com>
2024-04-13 02:55:13 +0530

    Dusted.

--- frontend/vite-project/src/components/Summary.jsx
+++ frontend/vite-project/src/components/Summary.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Loading from "../components/Loading"
 
 const Summary = () => {
   return (
     <section className='mt-6 flex-grow'>
         <h2 className='text-xl mb-4 font-semibold'>Generated Article</h2>
         <div id="content" className='mt-2 text-gray-800 space-y-4'>
-        <Loading  color="green"/>
         </div>
     </section>
   )
