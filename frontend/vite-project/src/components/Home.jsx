36b9a17c35c337d2407670dd80256742d5a4d217
ram <rramkumar1306@gmail.com>
2024-04-13 02:13:12 +0530

    Introduced summary.

--- frontend/vite-project/src/components/Home.jsx
+++ frontend/vite-project/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import homePage from "../contents/homePage.json";
+import Summary from "./Summary";
 
 const Home = () => {
   return (
@@ -9,14 +10,16 @@ const Home = () => {
           <h2 className="text-2xl font-semibold mb-4">{homePage.title}</h2>
           <p className="text-gray-800">{homePage.description}</p>
         </div>
-        <br></br>
-        <div>
+        
+        <div className="mt-6">
           <h2 className="text-xl font-semibold mb-4">Enter Youtube Video Link </h2>
           <div className="flex space-x-4">
             <input id="youtubeLink" type="url" placeholder="Paste Youtube Link" className="flex-grow p-4 border border-blue-900 rounded-lg"/>
             <button id="generateBtn" className="hover:bg-[#3A3A3A]">Generate</button>
           </div>
         </div>
+
+        <Summary/>
       </div>
     </div>
   );
---
+++ frontend/vite-project/src/components/Summary.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Summary = () => {
+  return (
+    <section className='mt-6 flex-grow'>Summary</section>
+  )
+}
+
+export default Summary
