4ae5301002ba63b154f9df4b0874320794ee5bac
ram <rramkumar1306@gmail.com>
2024-04-14 10:59:51 +0530

    Refactored.

--- backend/django_project/django_project/settings.py
+++ backend/django_project/django_project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'blog_generator',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
--- backend/django_project/django_project/urls.py
+++ backend/django_project/django_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login', include('blog_generator.urls'))
+    path('', include('blog_generator.urls'))
 ]
