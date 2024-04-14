ed251b7682ebeb603b552824380aeb813e5621e3
ram <rramkumar1306@gmail.com>
2024-04-14 13:46:35 +0530

    Refactored.

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,9 +1,14 @@
-import './App.css';
-import Home from './components/Home.jsx'
-import NavBar from './components/NavBar.jsx'
-import SignIn from './components/SignIn.jsx';
-import SignUp from './components/SignUp.jsx'
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import "./App.css";
+import Home from "./components/Home.jsx";
+import NavBar from "./components/NavBar.jsx";
+import SignIn from "./components/SignIn.jsx";
+import SignUp from "./components/SignUp.jsx";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -13,15 +18,14 @@ const router = createBrowserRouter(
       <Route path="/register" element={<SignUp />} />
     </Route>
   )
-)
+);
 function App() {
-
   return (
-    <div className='flex flex-col min-h-screen bg-gray100 font-sans antialiased'>
-      <NavBar/>
-      <RouterProvider router={router}/>
+    <div className="flex flex-col min-h-screen bg-gray100 font-sans antialiased">
+      <NavBar />
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
