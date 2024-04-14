8b649bb76754171d5eda3f05fa060d04263d92b4
ram <rramkumar1306@gmail.com>
2024-04-14 12:42:10 +0530

    Implemented react router.

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,14 +1,25 @@
-import { useEffect } from 'react';
 import './App.css';
 import Home from './components/Home.jsx'
 import NavBar from './components/NavBar.jsx'
+import SignIn from './components/SignIn.jsx';
+import SignUp from './components/SignUp.jsx'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index element={<Home />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+    </Route>
+  )
+)
 function App() {
 
   return (
     <div className='flex flex-col min-h-screen bg-gray100 font-sans antialiased'>
       <NavBar/>
-      <Home/>
+      <RouterProvider router={router}/>
     </div>
   )
 }
--- frontend/vite-project/src/components/Home.jsx
+++ frontend/vite-project/src/components/Home.jsx
@@ -6,34 +6,36 @@ import Loading from "../components/Loading";
 const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
   return (
-    <div className="flex-grow container mx-auto mt-10 px-4 sm:px-0">
-      <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105 flex flex-col">
-        <div>
-          <h2 className="text-2xl font-semibold mb-4">{homePage.title}</h2>
-          <p className="text-gray-800">{homePage.description}</p>
-        </div>
-
-        <div className="mt-6">
-          <h2 className="text-xl font-semibold mb-4">
-            Enter Youtube Video Link{" "}
-          </h2>
-          <div className="flex space-x-4">
-            <input
-              id="youtubeLink"
-              type="url"
-              placeholder="Paste Youtube Link"
-              className="flex-grow p-4 border border-blue-900 rounded-lg"
-            />
-            <button id="generateBtn" className="hover:bg-[#3A3A3A]">
-              Generate
-            </button>
+    <>
+      <div className="flex-grow container mx-auto mt-10 px-4 sm:px-0">
+        <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105 flex flex-col">
+          <div>
+            <h2 className="text-2xl font-semibold mb-4">{homePage.title}</h2>
+            <p className="text-gray-800">{homePage.description}</p>
           </div>
-        </div>
 
-        <Loading isLoading={isLoading}/>
-        <Summary />
+          <div className="mt-6">
+            <h2 className="text-xl font-semibold mb-4">
+              Enter Youtube Video Link{" "}
+            </h2>
+            <div className="flex space-x-4">
+              <input
+                id="youtubeLink"
+                type="url"
+                placeholder="Paste Youtube Link"
+                className="flex-grow p-4 border border-blue-900 rounded-lg"
+              />
+              <button id="generateBtn" className="hover:bg-[#3A3A3A]">
+                Generate
+              </button>
+            </div>
+          </div>
+
+          <Loading isLoading={isLoading} />
+          <Summary />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
