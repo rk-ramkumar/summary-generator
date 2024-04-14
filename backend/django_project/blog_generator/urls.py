c6448a86341a6a46ca286bd80ee6e27a47265b2f
ram <rramkumar1306@gmail.com>
2024-04-14 14:07:21 +0530

    Introduced register endPoint.

--- backend/django_project/blog_generator/urls.py
+++ backend/django_project/blog_generator/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("api/login/", views.user_login, name="login" )
-]
+    path('api/register/', views.user_register, name="register")
+],
--- backend/django_project/blog_generator/views.py
+++ backend/django_project/blog_generator/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 import json
+
+Users = get_user_model()
 
 @api_view(['POST'])
 def user_login(request):
@@ -15,6 +17,20 @@ def user_login(request):
     
     if user is not None:
         login(request, user)
-        return JsonResponse({'success': 'User authenticated successfully'})
+        return JsonResponse({'success': 'User authenticated successfully', 'status': "success"})
     else:
-        return JsonResponse({'error': 'Invalid username or password'})
+        return JsonResponse({'error': 'Invalid username or password', 'status': 'error'})
+    
+@api_view(['POST'])
+def user_register(request):
+    data = json.loads(request.body)
+    
+    email = data.get("email")
+    password = data.get("password")
+    
+    if Users.objects.filter(email=email).exists():
+        return JsonResponse({'error': 'User already exist', 'status': 'error'})
+    
+    user = Users.object.create_user(email=email, password=password)
+    
+    return JsonResponse({'success': 'User created successfully', 'status': 'success'})
