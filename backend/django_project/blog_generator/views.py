b9833658bcc76da3b6fdc9a2428dae632ccebda2
ram <rramkumar1306@gmail.com>
2024-04-14 19:58:01 +0530

    Introduced generate blog endpoint.

--- backend/django_project/blog_generator/views.py
+++ backend/django_project/blog_generator/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate, login, get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from pytube import YouTube
 import json
 
 Users = get_user_model()
@@ -36,3 +39,19 @@ def user_register(request):
     user.save()
     
     return JsonResponse({'success': 'User created successfully', 'status': 'success'})
+
+@csrf_exempt
+@api_view(['POST'])
+def generateBlog(request):
+    try:
+        data = json.loads(request.body)
+
+        ytLink = data.link
+    except (KeyError, json.JSONDecodeError):
+        return JsonResponse({"Error": "Invaild data"}, status = 400)
+    
+
+def extractLink(link):
+    yt = YouTube(link)
+    title = yt.title
+    
